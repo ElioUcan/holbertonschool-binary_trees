d170b37fca859c4fcd822ee93c9afb67e277601f
Elio Ucan <eliozapata2006@gmail.com>
2026-08-03 15:58:00 -0600

    commit

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts the leaves in a btree
+ * @tree: btree
+ * Return: size_t
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+size_t left_leaves = 0;
+size_t right_leaves = 0;
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+{
+return (1);
+}
+left_leaves = binary_tree_leaves(tree->left);
+right_leaves = binary_tree_leaves(tree->right);
+return (left_leaves + right_leaves);
+}
