8a7bf299dec47fc14cce37445a0cb0b735195cac
Elio Ucan <eliozapata2006@gmail.com>
2026-08-03 16:02:20 -0600

    commit

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * custom_height -  measures the balance factor of a binary tree
+ * @tree: btree
+ * Return: size_t
+ */
+size_t custom_height(const binary_tree_t *tree)
+{
+size_t left_height = 0;
+size_t right_height = 0;
+if (tree == NULL)
+return (0);
+left_height = custom_height(tree->left);
+right_height = custom_height(tree->right);
+if (left_height > right_height)
+return (left_height + 1);
+return (right_height + 1);
+}
+int binary_tree_balance(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+return (custom_height(tree->left) - custom_height(tree->right));
+}
