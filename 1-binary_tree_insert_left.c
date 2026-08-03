dfe8df7e954a6c2b080360fbfe2c171cef49efad
Elio Ucan <eliozapata2006@gmail.com>
2026-08-03 15:00:47 -0600

    commit

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node -  inserts a node as the left-child of another node
+ * @parent: B-tree
+ * @value: int
+ * Return: B-tree
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *leftnode;
+binary_tree_t *temp;
+
+if (parent == NULL)
+return (NULL);
+
+temp = parent;
+
+leftnode = malloc(sizeof(binary_tree_t));
+if (leftnode == NULL)
+return (NULL);
+
+temp->left = leftnode;
+
+leftnode->parent = parent;
+leftnode->n = value;
+leftnode->left = NULL;
+leftnode->right = NULL;
+
+return (leftnode);
+}
