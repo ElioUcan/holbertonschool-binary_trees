a7afbc7840f90d232c4ea74fdbf9e5df1f24ce28
Elio Ucan <eliozapata2006@gmail.com>
2026-08-03 15:26:47 -0600

    commit

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes a b-tree
- * @tree: B-tree
- * Return: void
+ * binary_tree_is_leaf - check if node is a leaf
+ * @node: b-tree
+ * Return: 1 if leaf  if no 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 if (node->parent == NULL)
-return (1);
-
 return (0);
+
+return (1);
 }
