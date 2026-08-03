e4b7cef42eb7a4cf74fa03caf03ba1700a3a3837
Elio Ucan <eliozapata2006@gmail.com>
2026-08-03 15:17:13 -0600

    commit

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left -  inserts a node as the left-child of another node
+ * binary_tree_insert_right -  inserts a node as the right-child of another node
  * @parent: B-tree
  * @value: int
  * Return: B-tree
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *rightnode;
 if (parent == NULL)
