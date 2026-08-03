c1c680dbb733724ccbd3f736b8efdea20be70a01
Elio Ucan <eliozapata2006@gmail.com>
2026-08-03 15:34:23 -0600

    commit

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if node is a root
+ * @node: b-tree
+ * Return: 0 if leaf  if no 1
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (node == NULL)
+return (0);
+
+if (node->parent == NULL)
+{
+if (node->left == NULL && node->right == NULL)
+return (0);
+
+return (1);
+}
+return (0);
+}
